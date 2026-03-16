481230237e739ff2b4eb2704569fd32f707adf4b
mrwhats-it <reachmathewka@gmail.com>
2026-03-16 08:43:37 +0530

    changed get_all_songs to get_all_songs()

--- streaming/music_manager.py
+++ streaming/music_manager.py
@@ -6,7 +6,7 @@ def get_all_songs():
     return [f for f in os.listdir(MUSIC_FOLDER) if f.endswith(".mp3")]
 
 def song_exists(song_name):
-    return song_name in get_all_songs
+    return song_name in get_all_songs()
 
 def get_song_path(song_name):
     return os.path.join(MUSIC_FOLDER, song_name)
