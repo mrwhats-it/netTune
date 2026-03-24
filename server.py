ea6dd3f90d403bf640a0fc651bd74d80928801f6
mrwhats-it <reachmathewka@gmail.com>
2026-03-24 17:29:01 +0530

    added a part of multithreading server

--- server.py
+++ server.py
@@ -27,6 +27,17 @@ def read_data(client_socket,song_name):
             if data==b'':
                 break
             client_socket.sendall(data)
+
+def run_client(client_socket,client_address):
+    instruction=client_socket.recv(BUFFER_SIZE).decode("utf8")
+    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+    
+    #for now just take the filename
+    song_name=instruction.split()[1]
+
+    details=init_details(client_socket,song_name)
+    read_data(client_socket,song_name)
+    
         
 def run_server():
     # create a socket object
@@ -37,19 +48,14 @@ def run_server():
     port = 8000
 
     server.bind((server_ip, port))
-    
-    server.listen(0)
     print(f"Listening on {server_ip}:{port}")
-
-    client_socket, client_address = server.accept()
-    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-    instruction=client_socket.recv(BUFFER_SIZE).decode("utf8")
     
-    #for now just take the filename
-    song_name=instruction.split()[1]
+    while True:
+        server.listen(4)
+        client_socket, client_address = server.accept()
+        newClientThread=threading.Thread(target=run_client,args=(client_socket,client_address))
+        newClientThread.start()
 
-    details=init_details(client_socket,song_name)
-    read_data(client_socket,song_name)
     
-
+    
 run_server()
